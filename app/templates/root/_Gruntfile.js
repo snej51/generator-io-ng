8422b80ff655de3beacf4b401b69c25dffabd9fb
Jens Oswald <oswald@infonline.de>
2014-10-01 11:56:53 +0200

    - grunt-open changed port to 9000

--- app/templates/root/_Gruntfile.js
+++ app/templates/root/_Gruntfile.js
@@ -511,7 +511,7 @@ module.exports = function(grunt) {
             },
             open: {
                 server: {
-                    path: 'http://localhost:<%= connect.options.port %>'
+                    path: 'http://localhost:9000'
                 }
             }
         }
